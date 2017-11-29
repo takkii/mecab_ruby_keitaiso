fe95f4737799da9e73f8812d58c7525c014c3d2f
Takayuki Kamiyama <karuma.reason@gmail.com>
2017-11-29 12:13:35 +0900

    簡潔にファイルを閉じました。

--- lib/version.rb
+++ lib/version.rb
@@ -1,9 +1,12 @@
 module Version
 	
 	def tools
-	puts 'rice is keitaiso_tools made in 2017.'
+		puts 'rice is keitaiso_tools made in 2017.'
 	end
+	
 	module_function :tools
 end
 
 Version.tools
+
+__END__
